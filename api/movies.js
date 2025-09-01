8cf8763751f97b0eb60b3aa1a1ddb97dc834781f
carlos-kennedy <carlosjustcarlos@outlook.com>
2025-09-01 19:23:06 -0300

    :sparkles: feat: Adding the serverless

---
+++ api/movies.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { name, year } = req.query;
+
+  try {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: process.env.API_KEY,
+        t: name,
+        y: year,
+      },
+    });
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar filmes" });
+  }
+}
